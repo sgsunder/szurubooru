5b5176eb872071cce5c69666f7f5321376494631
Shyam Sunder <sgsunder1@gmail.com>
2026-07-27 22:48:00 -0400

    nix: add testing to server package

--- server/package.nix
+++ server/package.nix
@@ -1,10 +1,37 @@
 {
   lib,
+  fetchurl,
   python3,
   ffmpeg-headless,
+  postgresql,
+  which,
 }: let
   src = ./.;
   version = (lib.importTOML (src + "/pyproject.toml")).project.version;
+
+  testing-postgresql = python3.pkgs.buildPythonPackage {
+    pname = "testing.postgresql";
+    version = "1.3.0";
+    format = "wheel";
+    src = fetchurl {
+      url = "https://files.pythonhosted.org/packages/11/76/d614d4bc950d961a73c952e9a2e0956d02d0869a86d3dfad070376863988/testing.postgresql-1.3.0-py2.py3-none-any.whl";
+      hash = "sha256-G0Ha65jfyM1KWEu5Ho9fSrGCmThw+VJXr+XxumFRpZg=";
+    };
+    doCheck = false;
+    propagatedBuildInputs = with python3.pkgs; [pg8000 testing-common-database];
+  };
+
+  pytest-pgsql = python3.pkgs.buildPythonPackage {
+    pname = "pytest-pgsql";
+    version = "1.1.2";
+    format = "wheel";
+    src = fetchurl {
+      url = "https://files.pythonhosted.org/packages/b5/68/7b44e25d51a2569ea74626a53072cc9c73388cb495bc3fc0c92157f9c809/pytest_pgsql-1.1.2-py3-none-any.whl";
+      hash = "sha256-RdEn759i0p5BgDpWlRKcCG13ZmNIcW2DeCq/13EIrPs=";
+    };
+    doCheck = false;
+    propagatedBuildInputs = with python3.pkgs; [freezegun pytest sqlalchemy testing-postgresql];
+  };
 
   pkg = python3.pkgs.buildPythonApplication {
     pname = "szurubooru-server";
@@ -13,6 +40,35 @@
 
     nativeBuildInputs = with python3.pkgs; [setuptools pythonRelaxDepsHook];
     pythonRemoveDeps = ["pillow-avif-plugin"];
+
+    nativeCheckInputs =
+      (with python3.pkgs; [
+        freezegun
+        pytest-cov
+        pytest-pgsql
+        pytest-xdist
+        pytestCheckHook
+        testing-postgresql
+      ])
+      ++ [
+        ffmpeg-headless
+        postgresql
+        which
+      ];
+
+    # szurubooru/config.py only reads POSTGRES_* env vars inside a container
+    # fake by setting container env variable
+    preCheck = ''
+      export container=nix-sandbox
+      export TEST_ENVIRONMENT=true
+      export HOME=$(mktemp -d)
+    '';
+
+    # restrict pytest thread count to prevent excessive simultaneous postgres queries
+    # which leads to slowdowns + timeout errors
+    dontUsePytestXdist = true;
+    pytestFlags = ["--tb=short" "--numprocesses=2"];
+
     propagatedBuildInputs = with python3.pkgs; [
       certifi
       coloredlogs
